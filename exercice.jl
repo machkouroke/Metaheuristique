387d6969a1d6befd1fc883cb7de09557bbb214f7
OKE   Machkour <40785379+machkouroke@users.noreply.github.com>
2023-04-12 17:43:08 +0000

    Init commit

---
+++ exercice.jl
@@ -0,0 +1,7 @@
+f(x) = x * sin(10 * pi * x) + 1
+function generate_population(pop_size::Int64)::Matrix{Int64}
+    # Generate a random population of size pop_size where each line of the Matrix is the binary representation of a number between 0 and 1
+    # Example: generate_population(2) = [0 0 0 0 0 0 0 0; 1 1 1 1 1 1 1 1]
+    # How to represent a number between 0 and 1 in binary in the code below:
+    return 
+end
--- main.jl
+++ main.jl
@@ -1,6 +1,9 @@
 using Random
+using ProgressBars
+using Statistics
+using Plots
 
-f(x::Int64) = sin(x/255 * pi)
+f(x::Int64) = sin(x / 255 * pi)
 """
     ⊛(x::Int64, p::Float64)
 Operator ⊛ est un opérateur de mutation. Il prend en entrée un bit et une probabilité de mutation.
@@ -11,14 +14,10 @@ Si la probabilité est inférieure à un nombre aléatoire entre 0 et 1, le bit 
 # Returns:
 - `Int64` est le bit muté
 """
-⊛(x::Int64, p::Float64)::Int64 = x ⊻ 1 if (rand() < p) else 0 ⊻ x
+⊛(x::Int64, p::Float64)::Int64 = (rand() < p) ? x ⊻ 1 : 0 ⊻ x
 
 function evaluate(x::Vector{Int64})::Float64
-    
-    evaluation = (join(x) |> x -> parse(Int64, x, base=2))
-    @show evaluation
-    @show typeof(evaluation)
-    return f(evaluation)
+    return f(join(x) |> x -> parse(Int64, x, base=2))
 end
 
 function randpop(popsize::Int64)::Matrix{Int64}
@@ -30,6 +29,15 @@ function select(popsize::Int64, n_parents::Int64)::Vector{Int64}
     return randperm(popsize)[1:n_parents]
 end
 
+function make_mates(population::Matrix{Int64}, n_parents::Int64, n_mates::Int64)::Tuple
+    mates::Tuple = ()
+    for _ in 1:n_mates
+        indices::Vector{Int64} = select(size(population)[1], n_parents)
+        mates = (mates..., (population[indices[1], :], population[indices[2], :]))
+    end
+    return mates
+end
+
 function crossover(parents::Matrix{Int64})::Vector{Int64}
     return [rand([x, y]) for (x, y) in zip(parents[1, :], parents[2, :])]
 end
@@ -39,28 +47,45 @@ function mutation(child::Vector{Int64})
     return [x ⊛ probability for x in child]
 end
 
-function survival(f, n_survivors::Int64, population::Matrix{Int64})
-    return 
+function survival(n_survivors::Int64, population::Matrix{Int64})
+    evaluations = mapslices(evaluate, population, dims=2)
+    indices_tries = sortperm(vec(evaluations), rev=true)
+    return population[indices_tries[1:n_survivors], :]
 end
 
 function eliminate_duplicates(population::Matrix{Int64})
     return unique(population, dims=1)
 end
 
-function plot_evolution(x::Matrix{Int64})
-    plot(x, evaluate.(x))
+function average_fitness(population::Matrix{Int64})
+    return mean(mapslices(evaluate, population, dims=2))
 end
-function main()
+function plot_evolution(max_fitness::Vector, min_fitness::Vector, avg_fitness::Vector)
+    plot(1:length(max_fitness), max_fitness, label="max fitness", xlabel="generation", ylabel="fitness", title="Evolution of the fitness")
+    plot!(1:length(min_fitness), min_fitness, label="min fitness")
+    plot!(1:length(avg_fitness), avg_fitness, label="avg fitness")
+end
+function main(debug::Bool=false)
     pop_size::Int64 = 5
     n_gen = 5
-    for _ in 1:n_gen
-        population = randpop(pop_size) |> eliminate_duplicates 
-        parents = population[select(pop_size, 2), :]
-        child = crossover(parents)
-        population = [population; child]
-        population = mutation.(population) |>  x -> survival(evaluate, pop_size, x)
-    
-        population = eliminate_duplicates(population)
-        println(population)
+    n_parents = 2
+    n_mates = 50
+    max_fitness = []
+    min_fitness = []
+    avg_fitness = []
+
+    for _ in tqdm(1:n_gen)
+        population::Matrix{Int64} = randpop(pop_size)
+        mates::Tuple = make_mates(population, n_parents, n_mates)
+        children = [crossover(Matrix(hcat(mates[i]...)')) for i in 1:n_mates] |> x -> Matrix(hcat(x...)') |> x -> eliminate_duplicates(x)
+        # display(children)
+        population = [population; children] |> x -> survival(pop_size, x)
+     
+        max_fitness = [max_fitness; maximum(mapslices(evaluate, population, dims=2))]
+        min_fitness = [min_fitness; minimum(mapslices(evaluate, population, dims=2))]
+        avg_fitness = [avg_fitness; average_fitness(population)]
     end
+    plot_evolution(max_fitness, min_fitness, avg_fitness)
 end
+
+main()
